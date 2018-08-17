e8ef69a1c5d24052e131dd05499079bb2d4844d0
unknown <samargiudan@gmail.com>
2018-08-16 21:43:08 -0400

    adding initial boilerplate to work with a go project

---
+++ cmd/runsim/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/dsamar/rpgbattlesim/pkg/helloworld"
+
+func main() {
+	helloworld.SayHello()
+}
---
+++ pkg/helloworld/sayhello.go
@@ -0,0 +1,7 @@
+package helloworld
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("hello world")
+}
